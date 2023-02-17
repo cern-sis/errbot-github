173fe822576219550ca5901cf10635bd2a1de319
jimil749 <jimildesai42@gmail.com>
2023-02-17 17:37:16 +0100

    cleanup: remove boilerplate
    Signed-off-by: jimil749 <jimildesai42@gmail.com>

--- githubzulip.py
+++ githubzulip.py
@@ -7,69 +7,6 @@ from urllib.parse import urlencode, quote_plus
 
 
 class Githubzulip(BotPlugin):
-    """
-    Plugin to recieve webhooks from Github and route it to Zulip
-    """
-    def activate(self):
-        """
-        Triggers on plugin activation
-
-        You should delete it if you're not using it to override any default behaviour
-        """
-        super(Githubzulip, self).activate()
-
-    def deactivate(self):
-        """
-        Triggers on plugin deactivation
-
-        You should delete it if you're not using it to override any default behaviour
-        """
-        super(Githubzulip, self).deactivate()
-
-    def get_configuration_template(self):
-        """
-        Defines the configuration structure this plugin supports
-
-        You should delete it if your plugin doesn't use any configuration like this
-        """
-        return {
-            "EXAMPLE_KEY_1": "Example value",
-            "EXAMPLE_KEY_2": ["Example", "Value"],
-        }
-
-    def check_configuration(self, configuration):
-        """
-        Triggers when the configuration is checked, shortly before activation
-
-        Raise a errbot.ValidationException in case of an error
-
-        You should delete it if you're not using it to override any default behaviour
-        """
-        super(Githubzulip, self).check_configuration(configuration)
-
-    def callback_connect(self):
-        """
-        Triggers when bot is connected
-
-        You should delete it if you're not using it to override any default behaviour
-        """
-        pass
-
-    def callback_message(self, message):
-        """
-        Triggered for every received message that isn't coming from the bot itself
-
-        You should delete it if you're not using it to override any default behaviour
-        """
-        pass
-
-    def callback_botmessage(self, message):
-        """
-        Triggered for every message that comes from the bot itself
-
-        You should delete it if you're not using it to override any default behaviour
-        """
-        pass
     def get_user(self, gh):
         gh_u = ""
         client = zulip.Client(site="https://cern-rcs-sis.zulipchat.com",
