d6747ced482a446028ba76582f63235ea1b42622
Ben <benjamin.bergia@gmail.com>
2023-06-01 11:50:02 +0200

    Fix file extension

---
+++ renderers/issue.py
@@ -0,0 +1,68 @@
+def render(payload):
+    user = payload["sender"]["login"]
+    issue = payload["issue"]
+
+    match payload["action"]:
+        case "assigned":
+            return f"""
+        {user} assigned this issue to {[a['login'] for a in payload['assignees']]}.
+        """
+
+        case "closed":
+            return f"Closed by {user}."
+
+        case "deleted":
+            return f"Deleted by {user}."
+
+        case "demilestoned":
+            milestone = issue["milestone"]
+            link = f"[{milestone['title']}]({milestone['html_url']})"
+
+            return f"""
+        {user} removed this issue from the milestone {link}.
+        """
+
+        case "edited":
+            return None
+
+        case "labeled":
+            return f"{user} added label {issue['label']['name']}."
+
+        case "locked":
+            return False
+
+        case "milestoned":
+            milestone = issue["milestone"]
+            link = f"[{milestone['title']}]({milestone['html_url']})"
+
+            return f"""
+        {user} added this issue to the milestone {link}.
+        """
+
+        case "opened":
+            return f"""
+      {user} opened issue [{issue['id']}]({issue['html_url']}).
+      {issue['title']}
+      {issue['body']}
+      """
+
+        case "pinned":
+            return False
+
+        case "reopened":
+            return f"Re-opened by {user}."
+
+        case "transferred":
+            return None
+
+        case "unassigned":
+            return f"{user} unassigned this issue."
+
+        case "unlabeled":
+            return f"{user} removed label {issue['label']['name']}."
+
+        case "unlocked":
+            return False
+
+        case "unpinned":
+            return False
