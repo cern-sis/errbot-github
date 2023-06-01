fcbe50fec87f6f31ad2754a137990519e4a7b773
Ben <benjamin.bergia@gmail.com>
2023-06-01 15:42:14 +0200

    forward returned values

--- renderers/__init__.py
+++ renderers/__init__.py
@@ -4,6 +4,6 @@ from . import issue
 def render(logger, payload):
     if "issue" in payload:
         logger.info("Rendering an issue event")
-        issue.render(logger, payload)
+        return issue.render(logger, payload)
     else:
         return None
